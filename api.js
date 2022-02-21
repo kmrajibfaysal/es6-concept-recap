c81925355ebf533fc878bec7d5be374e6f1bfb27
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-02-22 03:37:42 +0600

    added dynamic data load using fetch api and made a button to show users

--- api.js
+++ api.js
@@ -1,11 +1,21 @@
 function loadUsers() {
     fetch('http://jsonplaceholder.typicode.com/users')
         .then((res) => res.json())
-        .then((users) => console.log(users));
+        .then((users) => displayUsers(users));
 }
 
 function loadPosts() {
     fetch('http://jsonplaceholder.typicode.com/posts')
         .then((res) => res.json())
         .then((post) => console.log(post));
+}
+
+function displayUsers(users) {
+    const ul = document.querySelector('#users');
+    // eslint-disable-next-line no-restricted-syntax
+    for (const user of users) {
+        const li = document.createElement('li');
+        li.innerText = `Name: ${user.name}, Email: ${user.email}`;
+        ul.appendChild(li);
+    }
 }
