68180bef62089410debb94a8529e724d357b1dac
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-02-22 12:23:22 +0600

    added hw files of programming hero course about api and http request

--- api.js
+++ api.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 function loadUsers() {
     fetch('http://jsonplaceholder.typicode.com/users')
         .then((res) => res.json())
@@ -19,3 +20,22 @@ function displayUsers(users) {
         ul.appendChild(li);
     }
 }
+// const stuInfo = JSON.stringify({ name: 'James', roll: 3 });
+// console.log(stuInfo.name);
+
+// const premikas = { name: 'keka ferdousi', cars: { brand: 'toyota' } };
+
+// const { brand } = premikas.cars;
+
+// console.log(brand);
+// ____?____('https://jsonplaceholder.typicode.com/posts/1')
+// .then(res => res.json())
+//     .then(json => console.log(json));
+
+//     fetch('https://jsonplaceholder.typicode.com/todo/1')
+// .then(response => response ____?___ )
+// .then(json => console.log(json))
+
+const info = { data: [{ language: 'Javascript', library: 'React' }] };
+
+console.log(info.data[0].library);
