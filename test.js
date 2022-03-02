97b9785d574ca49e66e38cd052beccc74dd2836f
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-03-03 00:17:21 +0600

    added test files

---
+++ test.js
@@ -0,0 +1,17 @@
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// setTimeout(() => console.log(6));
+// setTimeout(() => console.log('dekhen'));
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+// console.log(7);
+// console.log(8);
+
+let count = 0;
+for (let i = 0; i < 10; i++) {
+    if (count < 8) {
+        console.log(count++);
+    }
+}
